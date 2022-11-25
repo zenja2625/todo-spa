1d76069490668dfbaa88466096218ee3394c29a9
Евгений Леонов <zenja2625@gmail.com>
2022-11-25 14:28:53 +0000

    Update store.ts serialize error

--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import { accountSlice } from './slices/accountSlice'
-import { appSlice } from './slices/appSlice'
-import { categoriesSlice } from './slices/categoriesSlice'
-import { todosSlice } from './slices/todosSlice'
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { accountSlice } from "./slices/accountSlice";
+import { appSlice } from "./slices/appSlice";
+import { categoriesSlice } from "./slices/categoriesSlice";
+import { todosSlice } from "./slices/todosSlice";
 
 export const store = configureStore({
-    reducer: {
-        app: appSlice.reducer,
-        account: accountSlice.reducer,
-        categories: categoriesSlice.reducer,
-        todos: todosSlice.reducer
-    }
-})
+  reducer: {
+    app: appSlice.reducer,
+    account: accountSlice.reducer,
+    categories: categoriesSlice.reducer,
+    todos: todosSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
