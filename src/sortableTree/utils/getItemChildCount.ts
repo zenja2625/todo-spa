bf2a75137a9f4e6fba8ce33e2b82cfd2a11dadfc
Евгений <zenja2625@gmail.com>
2023-02-25 17:32:40 +0200

    delete

--- src/sortableTree/utils/getItemChildCount.ts
+++
@@ -1,9 +0,0 @@
-import { TreeItem } from '../types'
-
-export const getItemChildCount = <T extends TreeItem>(items: Array<T>, index: number) => {
-    let count = 0
-    for (let i = index + 1; i < items.length && items[index].depth < items[i].depth; i++)
-        count++
-
-    return count
-}
--- src/sortableTree/utils/getScrollable.ts
+++
@@ -1,9 +0,0 @@
-export const getScrollable = (element: HTMLElement): HTMLElement | null => {
-    if (element.scrollHeight > element.clientHeight) {
-      return element
-    } else if (element.parentElement) {
-      return getScrollable(element.parentElement)
-    }
-
-    return null
-}
